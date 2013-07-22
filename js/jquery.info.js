903977595ab5f13ab44356fd69b11855b0be2e2a
nareynolds <nathaniel.reynolds@gmail.com>
2013-07-22 16:05:39 -0400

    Add template page for teaching material.

--- js/jquery.info.js
+++ js/jquery.info.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 	$("#info_button").click(function(){
 	
-		var targetUrl = $(location).attr('href') + 'info/';
+		var targetUrl = $(location).attr('href') + 'info/template.html';
 		//_ATLAS_.infoWindow = window.open();
 		//_ATLAS_.infoWindow.location = targetUrl;
 		document.getElementById('info_frame').contentWindow.location = targetUrl;
